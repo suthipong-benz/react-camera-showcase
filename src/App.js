ad6e6f5c9314ca84b31bb2f83008877acd8b139f
WEgital Suthipong.N <suthipong.n@WEgitals-MacBook-Air.local>
2022-11-24 11:13:49 +0700

    feat: upload image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 function App() {
+
+  const [selectedImage, setSelectedImage] = useState();
+  const [previewImage, setPreviewImage] = useState();
+
+  useEffect(() => {
+    if (!selectedImage) {
+      setPreviewImage(undefined);
+      return;
+    }
+
+    const ou = URL.createObjectURL(selectedImage)
+    setPreviewImage(ou);
+
+    return () => URL.revokeObjectURL(ou);
+  }, [selectedImage]);
+
+  const changeHandler = (e) => {
+    if (!e.target.files || e.target.files.length === 0) {
+      setSelectedImage(undefined);
+      return;
+    }
+    
+    setSelectedImage(e.target.files[0]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App py-5">
+      <Container className='text-start'>
+        <Row className='justify-content-center'>
+          <Col xs='12' md='8'>
+
+            {previewImage && (
+              <div className='my-3'>
+                <img src={previewImage} alt='' />
+              </div>
+            )}
+
+            <Form>
+              <FormGroup>
+                <Label for="uploadFile">
+                  File
+                </Label>
+
+                <Input
+                  id="uploadFile"
+                  name="file"
+                  type="file"
+                  accept="image/*"
+                  capture
+                  onChange={changeHandler}
+                />
+                <FormText>
+                  This is some placeholder block-level help text for the above input. It‘s a bit lighter and easily wraps to a new line.
+                </FormText>
+              </FormGroup>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
